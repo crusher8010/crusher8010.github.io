250881cd951de5ff9aad20729c04dd9355cb9444
crusher8010 <crusher8010@gmail.com>
2023-08-13 13:36:21 +0530

    added footer icons

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 import "./Footer.css";
+import { Link } from "@chakra-ui/react";
 
 function Footer() {
     return (
         <div className="footer">
+            <div>
+                <span className="profile-links">
+                    <Link href="https://github.com/crusher8010" isExternal><i className="fa-brands fa-github"></i></Link>
+                    <Link href="https://www.linkedin.com/in/anirudha-mandal-b3ab5a18a/" isExternal><i className="fa-brands fa-linkedin"></i></Link>
+                </span>
+            </div>
             <div className="footerDiv">
                 <a href="#homepage" ><h3>Home</h3></a>
                 <a href="#aboutme" ><h3>About Me</h3></a>
